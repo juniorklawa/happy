8cabebbd0d8d4468cfe8b488c18b024a4e195b76
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-12 16:56:30 -0300

    Confiure file to recive routes from routes folder

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import Routes from './routes';
+
 import './styles/global.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Routes />
     </div>
   );
-}
+};
 
 export default App;
