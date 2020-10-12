3bb05f2bdae38fdae87d0d19ba3ce59f93266eb5
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-12 13:38:43 -0300

    Creation files for dashboard page

---
+++ web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../../images/logo.svg';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+
+  );
+};
+
+export default Dashboard;
