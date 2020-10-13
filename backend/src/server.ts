24102839e6dbf77ce733685d0a3ff9f8f4f54201
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-13 10:12:01 -0300

    Basic route and instalation of libs for server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Opa'})
+})
+
 app.listen(3333);
