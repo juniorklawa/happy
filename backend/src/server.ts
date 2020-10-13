ec473ebaf475e3d797c502c44e8b7076b2c490f6
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-13 12:10:58 -0300

    Configuration of app server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import routes from './routes';
 
+import './database/connection';
+
 const app = express();
 
 app.use(express.json());
 app.use(routes)
-
-app.get('/', (request, response) => {
-  response.json({ message: 'Opa'})
-})
 
 app.listen(3333);
