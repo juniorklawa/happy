3e4f05a15d9be30b772dc3e1b1715b99e8105fec
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-14 11:25:39 -0300

    Add routes for Projects List and About pages

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing';
 import ProjectsMap from '../pages/ProjectsMap';
+import ProjectsList from '../pages/ProjectsList';
+import About from '../pages/About';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -12,6 +14,8 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/map" component={ProjectsMap} />
+        <Route path="/list" component={ProjectsList} />
+        <Route path="/about" component={About} />
       </Switch>
     </BrowserRouter>
   );
