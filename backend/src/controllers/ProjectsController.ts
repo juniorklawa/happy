582a75d7df424f138a02c4671c7cedc2b8ccdd18
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-13 22:53:56 -0300

    Configure image upload in some routes

--- backend/src/controllers/ProjectsController.ts
+++ backend/src/controllers/ProjectsController.ts
@@ -1,24 +1,31 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import projectView from '../views/projects_view';
+
 import Project from '../models/Project';
 
 export default {
   async index(request: Request, response: Response) {
     const projectsRepository = getRepository(Project);
 
-    const projects = await projectsRepository.find();
+    const projects = await projectsRepository.find({
+      relations: ['images'],
+    });
 
-    return response.json(projects)
+    return response.json(projectView.renderMany(projects));
   },
 
   async show(request: Request, response: Response) {
-    const { id  } = request.params;
+    const { id } = request.params;
 
     const projectsRepository = getRepository(Project);
 
-    const project = await projectsRepository.findOneOrFail(id);
+    const project = await projectsRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
 
-    return response.json(project)
+    return response.json(projectView.render(project));
   },
 
   async create(request: Request, response: Response) {
@@ -30,10 +37,15 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
     } = request.body;
 
     const projectsRepository = getRepository(Project);
+
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename };
+    });
 
     const project = projectsRepository.create({
       name,
@@ -43,11 +55,12 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
-    })
+      open_on_weekends,
+      images,
+    });
 
     await projectsRepository.save(project);
 
     response.status(201).json(project);
-  }
-}
+  },
+};
