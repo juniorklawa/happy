62c8272a7f5a5349c5d00d718c191ad224e3bbca
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-14 15:54:28 -0300

    Fix page structure

--- web/src/pages/About/index.tsx
+++ web/src/pages/About/index.tsx
@@ -1,58 +1,66 @@
 import React from 'react';
+import Sidebar from '../../components/Sidebar';
 
 import { Container, Content } from './styles';
 
 const About: React.FC = () => {
   return (
     <Container>
+      <Sidebar />
+
       <Content>
-        <h1>Como surgiu o ---?</h1>
+        <p>Sobre nós</p>
 
-        <p>
-          O --- se originou de um outro projeto, o happy! O happy foi
-          desenvolvido na Next Level Week (NLW), uma evento em que um projeto é
-          desenvolvido do total zero em apenas uma semana. Esse evento é
-          promovido pela Rocketseat, uma startup sensacional que tem feito
-          milhares de alunos pelo mundo afora!
-        </p>
+        <section>
+          <h1>Como surgiu o ---?</h1>
 
-        <p>
-          O --- se originou da terceira edição da NLW. Na primeira, fiz a trilha
-          iniciante. Na segunda, fiz a trilha avançada, e vi outras pessoas
-          fazendo as suas próprias versões do projeto principal. Quis fazer o
-          meu projeto personalizado também, mas como eu estava iniciando com
-          React, fiz apenas personalizações simples e pensei em fazer minha
-          aplicação personalizada para a próxima edição.
-        </p>
+          <p>
+            O --- se originou de um outro projeto, o happy! O happy foi
+            desenvolvido na Next Level Week (NLW), uma evento em que um projeto
+            é desenvolvido do total zero em apenas uma semana. Esse evento é
+            promovido pela Rocketseat, uma startup sensacional que tem feito
+            milhares de alunos pelo mundo afora!
+          </p>
 
-        <p>
-          A próxima edição chegou, e agora estou conseguindo fazer tudo o que eu
-          queria! Estou muito feliz por poder concluir o projeto com boas
-          práticas de código, funcionalidades novas, e até conseguir fazer o
-          deploy da aplicação (deixar a aplicação online).
-        </p>
+          <p>
+            O --- se originou da terceira edição da NLW. Na primeira, fiz a
+            trilha iniciante. Na segunda, fiz a trilha avançada, e vi outras
+            pessoas fazendo as suas próprias versões do projeto principal. Quis
+            fazer o meu projeto personalizado também, mas como eu estava
+            iniciando com React, fiz apenas personalizações simples e pensei em
+            fazer minha aplicação personalizada para a próxima edição.
+          </p>
 
-        <p>
-          Você pode conferir o projeto original (happy) clicando{' '}
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://www.figma.com/file/fWpZHM1ZaKuxzl0cc0dxk6/Happy-Web-(Copy)?node-id=48603%3A210"
-          >
-            aqui
-          </a>
-          ! E para ver o meu projeto, é só explorar esse site todinho! Espero
-          que você goste, e sou aberto à qualquer feedback, sugestão e críticas.
-          Caso queira falar comigo, me mande uma mensagem no linkedin clicando{' '}
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://www.linkedin.com/guilherme-illescas"
-          >
-            aqui
-          </a>
-          !
-        </p>
+          <p>
+            A próxima edição chegou, e agora estou conseguindo fazer tudo o que
+            eu queria! Estou muito feliz por poder concluir o projeto com boas
+            práticas de código, funcionalidades novas, e até conseguir fazer o
+            deploy da aplicação (deixar a aplicação online).
+          </p>
+
+          <p>
+            Você pode conferir o projeto original (happy) clicando{' '}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.figma.com/file/fWpZHM1ZaKuxzl0cc0dxk6/Happy-Web-(Copy)?node-id=48603%3A210"
+            >
+              aqui
+            </a>
+            ! E para ver o meu projeto, é só explorar esse site todinho! Espero
+            que você goste, e sou aberto à qualquer feedback, sugestão e
+            críticas. Caso queira falar comigo, me mande uma mensagem no
+            linkedin clicando{' '}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.linkedin.com/guilherme-illescas"
+            >
+              aqui
+            </a>
+            !
+          </p>
+        </section>
       </Content>
     </Container>
   );
--- web/src/pages/About/styles.ts
+++ web/src/pages/About/styles.ts
@@ -2,37 +2,45 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100vw;
-  height: 100vh;
-  padding: 64px 0;
+  min-height: 100vh;
 
-  background: #cedee5;
+  background: #f5f8fa;
   color: #4d6f80;
 `;
 
 export const Content = styled.div`
-  width: 1000px;
-  margin: 0 auto;
-  padding: 64px;
-  border-radius: 20px;
-
-  background: #fff;
-
-  color: #4d6f80;
-
-  h1 {
-    margin-bottom: 64px;
-    padding-bottom: 8px;
-    border-bottom: 1px solid #d3e2e5;
+  p {
+    color: #8fa7b2;
+    text-align: center;
+    font-size: 18px;
+    padding: 40px 0;
   }
 
-  p {
-    margin: 24px 0;
+  section {
+    width: 800px;
+    margin: 0 auto;
+    padding: 64px;
+    border-radius: 20px;
 
-    font-size: 18px;
-    line-height: 28px;
+    background: #fff;
 
-    a {
-      color: #5173a0;
+    color: #4d6f80;
+
+    h1 {
+      margin-bottom: 64px;
+      padding-bottom: 8px;
+      border-bottom: 1px solid #d3e2e5;
+    }
+
+    p {
+      margin: 24px 0;
+
+      font-size: 18px;
+      line-height: 28px;
+
+      a {
+        color: #5173a0;
+      }
     }
   }
 `;
