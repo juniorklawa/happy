1b1c29b3d44bc51618caa7d99fb2dc00cea6f541
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-15 09:07:08 -0300

    Add cors for localhost only

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './database/connection';
@@ -9,6 +10,11 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
