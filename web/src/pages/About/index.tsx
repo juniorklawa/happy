e54fea48e4bb4858634e4d902ff4f8a18e444989
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-17 20:26:42 -0300

    Update text

--- web/src/pages/About/index.tsx
+++ web/src/pages/About/index.tsx
@@ -12,49 +12,50 @@ const About: React.FC = () => {
         <p className="page-title">Sobre nós</p>
 
         <section>
-          <h1>Como surgiu o ---?</h1>
+          <h1>Como surgiu o happy?</h1>
 
           <p>
-            O --- se originou de um outro projeto, o happy! O happy foi
-            desenvolvido na Next Level Week (NLW), uma evento em que um projeto
-            é desenvolvido do total zero em apenas uma semana. Esse evento é
-            promovido pela Rocketseat, uma startup sensacional que tem feito
-            milhares de alunos pelo mundo afora!
+            O happy foi uma ramificação de um outro projeto, o happy! Perai,
+            deixa eu explicar melhor. O happy original foi desenvolvido na Next
+            Level Week (NLW), uma evento em que um projeto é desenvolvido do
+            total zero em apenas uma semana. Esse evento é promovido pela
+            Rocketseat, uma startup sensacional que tem feito milhares de alunos
+            pelo mundo afora!
           </p>
 
           <p>
-            O --- se originou da terceira edição da NLW. Na primeira, fiz a
-            trilha iniciante. Na segunda, fiz a trilha avançada, e vi outras
-            pessoas fazendo as suas próprias versões do projeto principal. Quis
-            fazer o meu projeto personalizado também, mas como eu estava
+            Já a minha versão do happy se originou da terceira edição da NLW. Na
+            primeira, fiz a trilha iniciante. Na segunda, fiz a trilha avançada,
+            e vi outras pessoas fazendo as suas próprias versões do projeto
+            principal. Quis fazer a minha versão também, mas como eu estava
             iniciando com React, fiz apenas personalizações simples e pensei em
             fazer minha aplicação personalizada para a próxima edição.
           </p>
 
           <p>
-            A próxima edição chegou, e agora estou conseguindo fazer tudo o que
-            eu queria! Estou muito feliz por poder concluir o projeto com boas
-            práticas de código, funcionalidades novas, e até conseguir fazer o
-            deploy da aplicação (deixar a aplicação online).
+            Bom, a próxima edição chegou, e agora estou conseguindo fazer tudo o
+            que eu queria! Estou muito feliz por poder concluir o projeto com
+            boas práticas de código, funcionalidades novas, e até conseguir
+            fazer o deploy da aplicação (deixar a aplicação online).
           </p>
 
           <p>
-            Você pode conferir o projeto original (happy) clicando{' '}
+            Você pode conferir o projeto original do happy clicando{' '}
             <a
               rel="noopener noreferrer"
               target="_blank"
               href="https://www.figma.com/file/fWpZHM1ZaKuxzl0cc0dxk6/Happy-Web-(Copy)?node-id=48603%3A210"
             >
               aqui
-            </a>
-            ! E para ver o meu projeto, é só explorar esse site todinho! Espero
-            que você goste, e sou aberto à qualquer feedback, sugestão e
-            críticas. Caso queira falar comigo, me mande uma mensagem no
-            linkedin clicando{' '}
+            </a>{' '}
+            (uma conta no Figma é necessária)! E para ver o meu projeto, é só
+            explorar esse site todinho! Espero que você goste, e sou aberto à
+            qualquer feedback, sugestão e críticas. Caso queira falar comigo, me
+            mande uma mensagem no linkedin clicando{' '}
             <a
               rel="noopener noreferrer"
               target="_blank"
-              href="https://www.linkedin.com/guilherme-illescas"
+              href="https://www.linkedin.com/in/guilherme-illescas"
             >
               aqui
             </a>
