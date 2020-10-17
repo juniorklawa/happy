2aaf3ac4f58151ef560aef4595a7a46a0aa7af11
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-17 20:29:51 -0300

    New page for list projects

--- web/src/pages/ProjectsList/index.tsx
+++ web/src/pages/ProjectsList/index.tsx
@@ -1,12 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form } from '@unform/web';
 
+import { Link } from 'react-router-dom';
 import Sidebar from '../../components/Sidebar';
 
+import api from '../../services/api';
+
 import { Container, Content } from './styles';
+import ProjectCard from '../../components/ProjectCard';
+
+interface Projects {
+  id: number;
+  name: string;
+  category: string;
+  about: string;
+  images: Array<{
+    id: number;
+    url: string;
+  }>;
+}
 
 const ProjectsList: React.FC = () => {
-  const [category, setCategory] = useState('');
+  const [projects, setProjects] = useState<Projects[]>([]);
+  // const [category, setCategory] = useState('');
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  // function handleSubmit(): void {
+  //   api
+  //     .get(`/projects`, {
+  //       params: {
+  //         category,
+  //       },
+  //     })
+  //     .then(response => {
+  //       setProjects(response.data);
+  //     });
+  // }
 
   return (
     <Container>
@@ -16,9 +50,46 @@ const ProjectsList: React.FC = () => {
         <p>Lista de projetos</p>
 
         <section>
-          <Form onSubmit={() => console.log('data')}>
+          {/* <Form onSubmit={handleSubmit}>
+            <div className="input-block">
+              <span>Categoria</span>
+              <select
+                value={category}
+                onChange={event => setCategory(event.target.value)}
+              >
+                <option
+                  value=""
+                  disabled
+                  defaultValue="Selecione uma opção"
+                  hidden
+                >
+                  Selecione uma opção
+                </option>
+                <option value="asilos">Asilos</option>
+                <option value="ambientalismo">Ambientalismo</option>
+                <option value="assistencia">
+                  Assistência e Serviços Sociais
+                </option>
+                <option value="educacao">Educação</option>
+                <option value="orfanato">Orfanato</option>
+                <option value="saude">Saúde</option>
+              </select>
+            </div>
+
             <button type="submit">Buscar</button>
-          </Form>
+          </Form> */}
+
+          {projects.map(project => {
+            return (
+              <Link key={project.id} to={`/projects/${project.id}`}>
+                <ProjectCard
+                  title={project.name}
+                  description={project.about}
+                  image={project.images[0].url}
+                />
+              </Link>
+            );
+          })}
         </section>
       </Content>
     </Container>
--- web/src/pages/ProjectsList/styles.ts
+++ web/src/pages/ProjectsList/styles.ts
@@ -20,21 +20,77 @@ export const Content = styled.div`
   }
 
   section {
-    height: 500px;
     background: #fff;
     border-radius: 20px;
+
+    padding: 32px 0;
+
+    a {
+      text-decoration: none;
+
+      img {
+        object-fit: cover;
+      }
+    }
   }
 
   form {
     display: flex;
-    align-items: center;
+    align-items: flex-end;
     justify-content: center;
 
+    padding: 32px;
+
+    .input-block {
+      flex: 1;
+    }
+
+    .input-block label,
+    .input-block span {
+      display: flex;
+      margin-bottom: 8px;
+
+      color: #8fa7b3;
+      line-height: 24px;
+    }
+
+    .input-block select {
+      width: 100%;
+      height: 48px;
+      padding: 0 16px;
+
+      background: #f5f8fa;
+      border: 1px solid #d3e2e5;
+      color: #5c8599;
+
+      border-radius: 20px;
+      outline: none;
+
+      cursor: pointer;
+    }
+
     button {
-      border: 0;
-      width: 120px;
-      border-radius: 16px;
-      outline: 0;
+      bottom: 0;
+      width: 150px;
+      height: 48px;
+
+      background: #f5f8fa;
+      border: 1px solid #d3e2e5;
+      outline: none;
+      color: #5c8599;
+
+      border-radius: 20px;
+      padding: 0 16px;
+      margin-left: 32px;
+
+      cursor: pointer;
+
+      transition: all 0.2s;
+    }
+
+    button:hover {
+      background: #d3e2e5;
+      border: 1px solid #5c8599;
     }
   }
 `;
